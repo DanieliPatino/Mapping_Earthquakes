116fc0ea8aaf1b978ebe410f4558329ba4250f2b
DanieliPatino <92958939+DanieliPatino@users.noreply.github.com>
2022-03-19 16:50:38 -0600

    13.4.3

--- Mapping_Lines/static/js/logic.js
+++ Mapping_Lines/static/js/logic.js
@@ -19,11 +19,7 @@ L.polyline(line, {
   }).addTo(map);
   
 
-<<<<<<< HEAD
 // We create the tile layer that will be the background of our map.
-=======
-// We create the second tile layer that will be the background of our map.
->>>>>>> 1fc63c269ad556f38a3b1425d7526e8b859de550
 let satelliteStreets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
   attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
   maxZoom: 18,
@@ -31,8 +27,4 @@ let satelliteStreets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/sate
 });
 
 // Then we add our 'graymap' tile layer to the map.
-<<<<<<< HEAD
 satelliteStreets.addTo(map);
-=======
-satelliteStreets.addTo(map);
->>>>>>> 1fc63c269ad556f38a3b1425d7526e8b859de550
